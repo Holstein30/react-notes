4b4855a85e031d48b817ee7e4780dbcdc895f107
Zarnerz <holstein30@live.com>
2019-04-09 15:04:29 -0400

    Note form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import React from "react";
 import "./app.css";
 
 class App extends React.Component {
+  render() {
+    return (
+      <h1>
+        <Note />
+      </h1>
+    );
+  }
+}
+
+class Note extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { message: "Howdy" };
+    this.state = { message: "Insert Note: " };
   }
 
   render() {
-    return <h1>{this.state.message}</h1>;
+    return (
+      <div>
+        <form action="/submitNote">
+          {this.state.message}
+          <input type="text" name="note" />
+          <input type="submit" value="Submit" />
+          <Display />
+        </form>
+      </div>
+    );
   }
 }
+
+const Display = () => {
+  return <h1>text</h1>;
+};
 
 export default App;

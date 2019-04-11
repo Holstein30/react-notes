969ad45781b1738de240c731036b95f0bfa91b0c
Dale Holstein <holstein30@live.com>
2019-04-10 20:54:20 -0400

    dummy data added

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const noteData = [
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  },
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  },
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  },
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  },
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  },
+  {
+    author: "test1",
+    message: "test1",
+    date: "test1"
+  }
+];

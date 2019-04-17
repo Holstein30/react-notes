e6edf28824e078949edeae7578e4043275b2059b
Dale Holstein <holstein30@live.com>
2019-04-17 08:25:51 -0400

    added notes state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      notes: []
+    };
     this.showSidebar = this.showSidebar.bind(this);
+    this.addNote = this.addNote.bind(this);
   }
 
   showSidebar(e) {
     e.preventDefault();
     this.sidebar.classList.toggle("show");
+  }
+
+  addNote(e) {
+    e.preventDefault();
+    console.log("submitted");
+    const note = {
+      title: this.noteTitle.value,
+      text: this.noteText.value
+    };
+    const newNotes = Array.from(this.state.notes);
+    newNotes.push(note);
+    this.setState({
+      notes: newNotes
+    });
   }
 
   render() {
@@ -35,15 +53,19 @@ class App extends React.Component {
               </div>
             </section>
             <aside className="sidebar" ref={ref => (this.sidebar = ref)}>
-              <form>
+              <form onSubmit={this.addNote}>
                 <h3>Add New Note</h3>
                 <div className="close-btn" onClick={this.showSidebar}>
                   <i className="fa fa-times" />
                 </div>
                 <label htmlFor="note-title">Title:</label>
-                <input type="text" name="note-title" />
+                <input
+                  type="text"
+                  name="note-title"
+                  ref={ref => (this.noteTitle = ref)}
+                />
                 <label htmlFor="note-text">Text:</label>
-                <textarea name="note-text" />
+                <textarea name="note-text" ref={ref => (this.noteText = ref)} />
                 <input type="submit" value="Add New Note" />
               </form>
             </aside>
